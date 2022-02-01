50b6ac8eb772dc64c19f59a3f4cb9d32833fda76
Ann <anngineer@gmail.com>
2022-02-01 18:21:27 -0500

    Adds event listeners to hyperlinks in dropdown nav to close the dropdown and changes the icon to a hamburger

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -7,3 +7,23 @@ document
       document.getElementById("right-nav").classList.add("dropdown");
     }
   });
+
+// JS for closing drop down menu when a link is clicked
+const linkPortfolio = document.getElementById("link-portfolio");
+const linkAbout = document.getElementById("link-about");
+const linkContact = document.getElementById("link-contact");
+
+linkPortfolio.addEventListener("click", () => {
+  closeMenu();
+});
+linkAbout.addEventListener("click", () => {
+  closeMenu();
+});
+linkContact.addEventListener("click", () => {
+  closeMenu();
+});
+function closeMenu() {
+  if (document.getElementById("right-nav").classList.contains("dropdown")) {
+    document.getElementById("right-nav").classList.remove("dropdown");
+  }
+}
